27ecc6ce4dac431ff7f2d7cacd41bcadbf824872
Maciej Lamberski <maciej@lamberski.com>
2014-10-28 20:25:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Device Mockups</title>
+    <link href="main.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="preview">
+      <div class="device">
+        <div class="ipad ipad-vertical">
+          <div class="screen">
+            <img src="http://placehold.it/1024x2000" alt="">
+          </div>
+          <div class="home"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="preview">
+      <div class="device">
+        <div class="ipad ipad-horizontal">
+          <div class="screen">
+            <img src="http://placehold.it/1024x2000" alt="">
+          </div>
+          <div class="home"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="preview">
+      <div class="device">
+        <div class="iphone iphone-vertical">
+          <div class="screen">
+            <img src="http://placehold.it/1024x2000" alt="">
+          </div>
+          <div class="home"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="preview">
+      <div class="device">
+        <div class="iphone iphone-horizontal">
+          <div class="screen">
+            <img src="http://placehold.it/1024x2000" alt="">
+          </div>
+          <div class="home"></div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Mockups
+
+Mockups is simple PHP tool for showing image of the website inside one of available devices. It can be useful to test what area of the layout is visible at once on various screen sizes.
+
+## Changes in styling
+
+Sass is used to generate CSS style. If you want to modify styling of or add new device, run below command to watch for changes in `main.scss` file.
+
+  sass --watch main.scss:main.css
+
+## License
+
+Mockups is licensed under MIT License.
